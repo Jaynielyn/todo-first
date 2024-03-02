cc4741841f227ccbdb5c2e2e31ef461405cc5aa6
jay <myacc.j2@gmail.com>
2024-03-02 15:59:12 +0900

    完了

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,4 +1,3 @@
-
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -8,8 +7,11 @@ use App\Http\Controllers\CategoryController;
 
 Route::get('/', [TodoController::class, 'index']);
 Route::post('/todos', [TodoController::class, 'store']);
-Route::patch('/todos/update', [TodoController::class, 'update']);
-Route::delete('/todos/delete', [TodoController::class, 'destroy']);
+Route::patch('/todos/{todo_id}', [TodoController::class, 'update']);
+Route::delete('/todos/{todo_id}', [TodoController::class, 'destroy']);
 
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::post('/categories', [CategoryController::class, 'store']);
+Route::patch('/categories/update', [CategoryController::class, 'update']);
+Route::delete('/categories/delete', [CategoryController::class, 'destroy']);
+Route::get('/todos/search', [TodoController::class, 'search']);
